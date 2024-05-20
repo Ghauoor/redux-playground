4a0d70865c1c442cf801abbcf3968c05d197d734
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-20 05:11:55 +0500

    Single Product Component

---
+++ src/components/e-commerce/singleProduct/SingleProduct.jsx
@@ -0,0 +1,21 @@
+import "./SingleProduct.css";
+
+function SingleProduct({ product }) {
+  return (
+    <div className="SingleProduct">
+      <img className="productImg" src={product.image} alt={product.title} />
+
+      <div className="productInfo">
+        <h2 className="productTitle">{product.title}</h2>
+        <p className="productPrice">${product.price.toFixed(2)}</p>
+      </div>
+      <div className="cartInfo">
+        <button className="button">-</button>
+        <h4>0</h4>
+        <button className="button">+</button>
+      </div>
+    </div>
+  );
+}
+
+export default SingleProduct;
