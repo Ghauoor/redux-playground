ce5932cc2dc7a3fadc5d43833a4ac11d1fe13f34
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-20 14:53:00 +0500

    cart Slice

---
+++ src/redux/slices/cartSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cartSlice",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      let cartItem = state.cart.find((item) => item.id === action.payload);
+      if (cartItem) {
+        cartItem.quantity += 1;
+      } else {
+        state.cart.push({
+          quantity: 1,
+          id: action.payload,
+        });
+      }
+    },
+
+    removeFromCart: (state, action) => {
+      let cartItem = state.cart.find((item) => item.id === action.payload);
+      if (cartItem) {
+        cartItem.quantity -= 1;
+        if (cartItem.quantity === 0) {
+          state.cart = state.cart.filter((item) => item.id !== action.payload);
+        }
+      }
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
